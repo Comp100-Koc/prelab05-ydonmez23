7bfd61b7fad9b5f4c7d1710c7a34db3038fbf0d0
ydonmez23 <ydonmez23@ku.edu.tr>
2026-04-03 11:22:23 +0300

    i did prelab

--- q1.py
+++ q1.py
@@ -2,4 +2,29 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    
+    a = len(s)
+    full = 0 
+    word = "" 
+    
+    for i in range(a):
+        start, stop = i, i 
+        while start >= 0 and stop < a and s[start] == s[stop]:
+            if (stop - start + 1) > full:
+                full = stop - start + 1
+                word = s[start:stop+1]
+            start -= 1
+            stop += 1
+            
+        start, stop = i, i + 1 
+        while start >= 0 and stop < a and s[start] == s[stop]: 
+            if (stop - start + 1) > full:
+                full = stop - start + 1
+                word = s[start:stop+1]
+            start -= 1
+            stop += 1
+
+    if full < 2:
+        return ""  
+
+    return word
--- q3b.py
+++ q3b.py
@@ -2,4 +2,32 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    if a.startswith('0b'):
+        a = a[2:]
+    if b.startswith('0b'):
+        b = b[2:]
+
+    a, b = fill_zeros(a, b)
+    
+    carry = 0
+    result = ''
+    for i in range(len(a)-1, -1, -1):
+        bit_sum = (int(a[i])) + (int(b[i])) + carry
+        result = str(bit_sum % 2) + result
+        carry = bit_sum // 2
+    if carry:
+        result = '1' + result
+    
+    i = 0
+    while i < len(result) and result[i] == '0':
+        i += 1
+    result = result[i:]
+    if not result:
+        result = '0'
+    return '0b' + result
+
+def fill_zeros(a, b):
+    maxlen = max(len(a), len(b))
+    a = ('0' * (maxlen - len(a))) + a
+    b = ('0' * (maxlen - len(b))) + b
+    return a, b
